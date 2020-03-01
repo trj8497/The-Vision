610153d43b0ae3e782ee7fd7fc052a2f47db5cb3
Tejaswini Jagtap <35979350+trj8497@users.noreply.github.com>
2020-03-01 05:25:53 -0500

    Update Image_Detector.py

--- The_Vision/Image_Detector.py
+++ The_Vision/Image_Detector.py
@@ -1,13 +1,21 @@
-import cv2
 from playsound import playsound
+from datetime import datetime
+from os import path as p
+import time as t
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Credentials_File.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Vision Project-9d1350f85714.json"
+
 from google.cloud import texttospeech
 from google.cloud import vision
+
 client = vision.ImageAnnotatorClient()
+counter = 0
+
 def text_to_speech(text):
+    global counter
     client = texttospeech.TextToSpeechClient()
     input_text = texttospeech.types.SynthesisInput(text=text)
+
     voice = texttospeech.types.VoiceSelectionParams(
         language_code='en-US',
         name='en-US-Standard-C',
@@ -15,32 +23,60 @@ def text_to_speech(text):
     audio_config = texttospeech.types.AudioConfig(
         audio_encoding=texttospeech.enums.AudioEncoding.MP3)
     response = client.synthesize_speech(input_text, voice, audio_config)  # The response's audio_content is binary.
-    with open('output.mp3', 'wb') as out:
+
+    with open('audio/output'+str(counter)+'.mp3', 'wb') as out:
         out.write(response.audio_content)
-        #print('Audio content written to file "output.mp3"')
+        print('Audio content written to file "output.mp3"')
+
 def detect_img(path):
+    global counter
     with open(path, 'rb') as image_file:
         content = image_file.read()
     image = vision.types.Image(content=content)
+
     objects = client.object_localization(
         image=image).localized_object_annotations
+
     print('Number of objects found: {}'.format(len(objects)))
+
     finalstr = ''
     for object_ in objects:
         print('\n{}'.format(object_.name))
         finalstr = finalstr + ' {}'.format(object_.name)
-        finalstr = finalstr + 'and'
+        finalstr = finalstr + '    '
+
     text_to_speech(finalstr)
-    playsound('output.mp3')
-img = cv2.VideoCapture(0)
+    playsound('audio/output'+str(counter)+'.mp3')
+    os.unlink('audio/output'+str(counter)+'.mp3')
+    counter += 1
+    # os.remove('output.mp3')
+# img = cv2.VideoCapture(0)
+
 while(True):
-    ret, frame = img.read()
-    path = 'live.png'
-    cv2.imwrite(path, frame)
-    cv2.imshow('frame', frame)
-    key = cv2.waitKey(1)
-    if key == ord('c'):
+
+    now = datetime.now()
+    time = ""
+    hour = str(now.strftime("%H"))
+    minute = str(now.strftime("%M"))
+    second = str(now.strftime("%S"))
+    time = time + (hour + "-" + minute + "-" + second)
+
+    path = 'Capture/{}.jpg'.format(time)
+    if p.exists(path):
         detect_img(path)
-        break
-img.release()
-cv2.destroyAllWindows()
+        os.unlink(path)
+
+    t.sleep(1)
+    # ret, frame = img.read()
+    # path = 'Capture/live.png'
+    # cv2.imwrite(path, frame)
+
+    # cv2.imshow('frame', frame)
+
+    # key = cv2.waitKey(1)
+    # if key == ord('c'):
+    # detect_img(path)
+        # break
+
+# img.release()
+# cv2.destroyAllWindows()
